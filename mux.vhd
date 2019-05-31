961a2b17aa2b556c5ffbb2195e90a8818643eecd
EricKurachi <erictkurachi@gmail.com>
2019-05-31 15:57:13 -0300

    identatacao corrigida

--- mux.vhd
+++ mux.vhd
@@ -3,13 +3,13 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 entity mux is
-	port(a, b, s : in  std_logic;
-		 x       : out std_logic);
+    port(a, b, s : in  std_logic;
+	 x       : out std_logic);
 end mux;
 
 architecture behavior of mux is
 begin 
-	with s select
-		x <= a when "0"
-			 b when "1"
+    with s select
+        x <= a when "0"
+	     b when "1"
 end behavior;
