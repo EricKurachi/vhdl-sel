fce54ab6bd83423891aea220154f4a95669b617b
lucdark8 <39463809+lucdark8@users.noreply.github.com>
2019-06-03 15:45:25 -0300

    Update mux.vhd

--- mux.vhd
+++ mux.vhd
@@ -2,14 +2,14 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
-entity mux is
+entity Mux is
     port(a, b, s : in  std_logic;
 	 x       : out std_logic);
-end mux;
+end Mux;
 
-architecture behavior of mux is
+architecture behavior of Mux is
 begin 
     with s select
-        x <= a when "0"
-	     b when "1"
+        x <= a when '0',
+	     b when '1';
 end behavior;
