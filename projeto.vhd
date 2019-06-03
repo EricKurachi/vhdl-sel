64c31fe20b4298c654c915a1345fecdd9b767b4a
lucdark8 <39463809+lucdark8@users.noreply.github.com>
2019-06-03 16:44:23 -0300

    Update projeto.vhd

--- projeto.vhd
+++ projeto.vhd
@@ -1,29 +1,50 @@
-entity logisticmap is
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+entity Gerador is
 	generic(
-		bussize	: integer := 16
-		);
+		bussize	: integer := 16);
 	port(
 		clk, clr : in  std_logic;
 		start	 : in  std_logic;
 		x0		 : in  std_logic_vector(bussize-1 downto 0);
-		xnout	 : out std_logic_vector(bussize-1 downto 0)
-		);
-end logisticmap;
+		xnout	 : buffer std_logic_vector(bussize-1 downto 0));	--na vdd, é um out std_logic_vector
+end Gerador;
 
-architecture 
-	
+architecture behavior of Gerador is
 
-------------------------------------------------------
+component Mux is
+    port(a, b, s : in  std_logic;
+	 x       : out std_logic);
+end component;
+
 --FliFlop tipo D
-PROCEDURE ffd (
-	SIGNAL d	:	IN  STD_LOGIC;
-	SIGNAL clkffd	:	IN  STD_LOGIC;
-	SIGNAL q	:	OUT STD_LOGIC) IS
-	BEGIN
-		WAIT UNTIL clkffd'EVENT AND clkffd = '1';
-		q <= d;
-	END ffd;
+procedure ffd (
+	signal d	:	in  std_logic;
+	signal clkffd	:	in  std_logic;
+	signal rst : in std_logic;
+	signal q	:	out std_logic) is
+	begin
+		if rst='1' then
+			q <= '0';
+		elsif clkffd'event AND clkffd = '1' then
+			q <= d;
+		end if;
+end ffd;
 
-FOR I IN 0 TO bussize-1 LOOP
-	ffd(aux(i),clk,xnout(i));
-END LOOP;
+signal aux : std_logic_vector(bussize-1 downto 0);
+--signal xfuture : std_logic_vector(bussize-1 downto 0);
+
+begin
+MUX_loop:
+	for i in bussize-1 downto 0 generate
+		MX: Mux port map(x0(i),xnout(i),start,aux(i));				----mudar no futuro para xfuture
+	end generate MUX_loop;
+
+
+FFDs_loop:
+	for i in bussize-1 downto 0 generate
+		ffd(aux(i),clk,clr,xnout(i));
+	end generate FFDs_loop;	
+end behavior;	
