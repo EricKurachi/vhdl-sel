f62d2277666c17487520a3358b3c3e0144122e39
lucdark8 <39463809+lucdark8@users.noreply.github.com>
2019-06-07 21:29:15 -0300

    Update mux.vhd

--- mux.vhd
+++ mux.vhd
@@ -3,14 +3,14 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 entity Mux is 
-	generic(N: natural := 3);
-   port( a, b   : in  std_logic_vector(N-1 downto 0);
-	   s	: in  std_logic;
-	   x    : out std_logic_vector(N-1 downto 0));
+	generic(N: natural := 8);
+   port( a, b  : in  std_logic_vector(N-1 downto 0);
+			s		: in  std_logic;
+		   x     : out std_logic_vector(N-1 downto 0));
 end Mux;
 
 architecture behavior of Mux is
 begin
-        x <= a when s='0' else
-	     b;
+        x <= a when s='1' else
+				     b;
 end behavior;
